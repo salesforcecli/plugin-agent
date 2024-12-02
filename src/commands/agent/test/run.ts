06706650f973af92d5495baceeec7c482f58117b
Willie Ruemmele <willieruemmele@gmail.com>
2024-12-02 08:48:10 -0700

    chore: make commands beta

--- src/commands/agent/test/run.ts
+++ src/commands/agent/test/run.ts
@@ -22,6 +22,7 @@ export default class AgentTestRun extends SfCommand<AgentTestRunResult> {
   public static readonly summary = messages.getMessage('summary');
   public static readonly description = messages.getMessage('description');
   public static readonly examples = messages.getMessages('examples');
+  public static state = 'beta';
 
   public static readonly flags = {
     'target-org': Flags.requiredOrg(),
