df16b9dbc5b4b1b7186f1e146ab2768f993b9f4c
Willie Ruemmele <willieruemmele@gmail.com>
2024-12-02 14:27:52 -0700

    chore: add agent preview command

---
+++ messages/agent.preview.md
@@ -0,0 +1,20 @@
+# summary
+
+Interact with an active agent, as a user would, to preview responses
+
+# description
+
+XXX
+
+# flags.name.summary
+
+The name of the agent you want to preview
+
+# flags.name.description
+
+the API name of the agent? (TBD based on agents library)
+
+# examples
+
+- <%= config.bin %> <%= command.id %> --agent HelpDeskAgent
+- <%= config.bin %> <%= command.id %> --agent ConciergeAgent --target-org production
---
+++ src/commands/agent/preview.tsx
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2024, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { SfCommand, Flags } from '@salesforce/sf-plugins-core';
+import { Messages } from '@salesforce/core';
+import React from 'react';
+import { render } from 'ink';
+import { AgentPreviewReact } from '../../components/agent-preview-react.js';
+
+Messages.importMessagesDirectoryFromMetaUrl(import.meta.url);
+const messages = Messages.loadMessages('@salesforce/plugin-agent', 'agent.preview');
+
+export default class AgentPreview extends SfCommand<void> {
+  public static readonly summary = messages.getMessage('summary');
+  public static readonly description = messages.getMessage('description');
+  public static readonly examples = messages.getMessages('examples');
+  public static readonly enableJsonFlag = false;
+  public static readonly requiresProject = true;
+
+  public static readonly flags = {
+    'target-org': Flags.requiredOrg(),
+    'api-version': Flags.orgApiVersion(),
+    name: Flags.string({
+      summary: messages.getMessage('flags.name.summary'),
+      description: messages.getMessage('flags.name.description'),
+      char: 'n',
+      required: true,
+    }),
+  };
+
+  public async run(): Promise<void> {
+    const { flags } = await this.parse(AgentPreview);
+    this.log(`previewing ${flags.name}`);
+
+    const instance = render(<AgentPreviewReact />);
+    await instance.waitUntilExit();
+  }
+}
