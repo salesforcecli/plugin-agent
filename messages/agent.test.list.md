a8b39fb514ac588ba0166ac6ffdc497edf52e2c9
Mike Donnalley <mdonnalley@salesforce.com>
2025-01-27 07:03:35 -0700

    feat(W-17669110): add list command (#63)
    * feat: add list command
    
    * chore: bump agents

---
+++ messages/agent.test.list.md
@@ -0,0 +1,11 @@
+# summary
+
+List the available tests in the org.
+
+# description
+
+Run this command to get a list of tests that are available in the org. This command will return the test ID, name, and created date of each test.
+
+# examples
+
+- <%= config.bin %> <%= command.id %>
---
+++ src/commands/agent/test/list.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2024, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { AgentTester, type AvailableDefinition } from '@salesforce/agents';
+import { SfCommand, Flags } from '@salesforce/sf-plugins-core';
+import { Messages } from '@salesforce/core';
+
+Messages.importMessagesDirectoryFromMetaUrl(import.meta.url);
+const messages = Messages.loadMessages('@salesforce/plugin-agent', 'agent.test.list');
+
+export type AgentTestListResult = AvailableDefinition[];
+
+export default class AgentTestList extends SfCommand<AgentTestListResult> {
+  public static readonly summary = messages.getMessage('summary');
+  public static readonly description = messages.getMessage('description');
+  public static readonly examples = messages.getMessages('examples');
+
+  public static readonly flags = {
+    'target-org': Flags.requiredOrg(),
+    'api-version': Flags.orgApiVersion(),
+  };
+
+  public async run(): Promise<AgentTestListResult> {
+    const { flags } = await this.parse(AgentTestList);
+
+    const agentTester = new AgentTester(flags['target-org'].getConnection(flags['api-version']));
+    const results = await agentTester.list();
+    this.table({
+      data: results,
+      columns: [
+        { key: 'fullName', name: 'Name' },
+        { key: 'id', name: 'Id' },
+        { key: 'createdDate', name: 'Created Date' },
+      ],
+      sort: { fullName: 'asc' },
+    });
+
+    return results;
+  }
+}
