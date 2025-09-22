0d178f99fe7c22729fc8216c401fe28f83c7e043
Willie Ruemmele <willieruemmele@gmail.com>
2025-09-22 09:59:14 -0600

    chore: address comments, bump dev-scripts, update license/headers

--- src/commands/agent/validate/authoring-bundle.ts
+++ src/commands/agent/validate/authoring-bundle.ts
@@ -1,8 +1,17 @@
 /*
- * Copyright (c) 2023, salesforce.com, inc.
- * All rights reserved.
- * Licensed under the BSD 3-Clause license.
- * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ * Copyright 2025, Salesforce, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
@@ -60,7 +69,7 @@ export default class AgentValidateAuthoringBundle extends SfCommand<AgentValidat
       };
     } catch (error) {
       // Handle validation errors
-      const err = error instanceof Error ? error : new Error(String(error));
+      const err = SfError.wrap(error);
       const formattedError = err.message
         .split('\n')
         .map((line) => `- ${line}`)
